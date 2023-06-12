7d1a9fefe97adf3b9655c9a1b2f0b554b9f45cee
Seungpyo Suh <107336329+sspzoa@users.noreply.github.com>
2023-06-12 20:33:30 +0900

    add cloud

--- lib/login_pages/loginpage.dart
+++ lib/login_pages/loginpage.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import '../widgets.dart';
 
 class LoginPage extends StatefulWidget {
   final VoidCallback onLogin;
@@ -11,10 +10,41 @@ class LoginPage extends StatefulWidget {
   _LoginPageState createState() => _LoginPageState();
 }
 
-class _LoginPageState extends State<LoginPage> {
+class _LoginPageState extends State<LoginPage> with TickerProviderStateMixin {
+  late AnimationController _controller1;
+  late AnimationController _controller2;
+  late AnimationController _controller3;
+  final cloudImageWidths = [150.0, 200.0, 100.0];
+  final cloudBottomOffsets = [600.0, 300.0, 200.0];
+  final cloudOpacities = [0.5, 0.3, 0.2];
+  final cloudImagePaths = [
+    'assets/images/loginpage/cloud.png',
+    'assets/images/loginpage/cloud.png',
+    'assets/images/loginpage/cloud.png',
+  ];
+  final cloudDurations = [
+    const Duration(seconds: 5),
+    const Duration(seconds: 6),
+    const Duration(seconds: 4),
+  ];
+
   @override
   void initState() {
     super.initState();
+    _controller1 = AnimationController(
+      duration: cloudDurations[0],
+      vsync: this,
+    )..repeat();
+
+    _controller2 = AnimationController(
+      duration: cloudDurations[1],
+      vsync: this,
+    )..repeat();
+
+    _controller3 = AnimationController(
+      duration: cloudDurations[2],
+      vsync: this,
+    )..repeat();
 
     SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
       statusBarColor: Colors.transparent,
@@ -25,51 +55,96 @@ class _LoginPageState extends State<LoginPage> {
   }
 
   @override
+  void dispose() {
+    _controller1.dispose();
+    _controller2.dispose();
+    _controller3.dispose();
+    super.dispose();
+  }
+
+  Widget buildCloudAnimation(int index) {
+    final AnimationController controller = index == 0
+        ? _controller1
+        : index == 1
+            ? _controller2
+            : _controller3;
+
+    return AnimatedBuilder(
+      animation: controller,
+      builder: (context, child) {
+        final opacity = cloudOpacities[index] +
+            (controller.value * (1 - cloudOpacities[index]));
+        final screenWidth = MediaQuery.of(context).size.width;
+        final offset = Offset(
+          (controller.value * (screenWidth + cloudImageWidths[index])) -
+              cloudImageWidths[index],
+          0,
+        );
+
+        return Opacity(
+          opacity: opacity,
+          child: Transform.translate(
+            offset: offset,
+            child: child,
+          ),
+        );
+      },
+      child: Image.asset(
+        cloudImagePaths[index],
+        width: cloudImageWidths[index],
+      ),
+    );
+  }
+
+  @override
   Widget build(BuildContext context) {
+    final screenWidth = MediaQuery.of(context).size.width;
+    const cloudImageWidth = 150.0;
+
     return Scaffold(
       backgroundColor: const Color(0xFF92B4CD),
-      body: Column(
+      body: Stack(
         children: [
-          Expanded(
-            child: Padding(
-              padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Center(
-                    child: Image.asset(
-                      'assets/images/icon1080.png',
-                      height: 275,
-                    ),
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 20),
-            child: Column(
-              children: [
-                InkWell(
-                  onTap: widget.onLogin,
-                  child: const CustomContainer(
-                    backgroundColor: Color(0xFFECD500),
-                    child: Center(
-                      child: Text(
-                        '카카오 계정으로 시작하기',
-                        style: TextStyle(
-                          fontSize: 20.0,
-                          fontWeight: FontWeight.bold,
-                          color: Color(0xFF381C1C),
+          Column(
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.fromLTRB(20, 0, 20, 20),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Center(
+                        child: Image.asset(
+                          'assets/images/icon1080.png',
+                          height: 275,
                         ),
                       ),
-                    ),
+                    ],
                   ),
                 ),
-                const SizedBox(height: 20),
-              ],
-            ),
+              ),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Column(
+                  children: [
+                    InkWell(
+                      onTap: widget.onLogin,
+                      child: Image.asset(
+                        'assets/images/loginpage/kakao_login_large_wide.png',
+                      ),
+                    ),
+                    const SizedBox(height: 50),
+                  ],
+                ),
+              ),
+            ],
           ),
+          for (var i = 0; i < cloudImageWidths.length; i++)
+            Positioned(
+              bottom: cloudBottomOffsets[i],
+              left: 0,
+              child: buildCloudAnimation(i),
+            ),
         ],
       ),
     );
